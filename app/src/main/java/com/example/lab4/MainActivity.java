d4cca8f5a74baaf1168f54014e00468b5c3ccf2c
jiaxinli <jli2274@wisc.edu>
2021-10-10 22:28:25 -0500

    Initial commit

---
+++ app/src/main/java/com/example/lab4/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.lab4;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
+
+//import android.service.controls.ControlsProviderService.TAG;
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private Button startButton;
+    private volatile boolean stopThread = false;
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.startButton);
+        textView = (TextView)findViewById(R.id.textView);
+    }
+
+    public void mockFiledownloader(){
+        //textView = (TextView)findViewById(R.id.textView);
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                startButton.setText("DOWNLOADING...");
+
+            }
+        });
+
+        for (int downloadProgress = 0; downloadProgress <= 100;downloadProgress=downloadProgress + 10){
+            if(stopThread){
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        startButton.setText("START");
+                    }
+                });
+                return;
+            }
+            //Log.d(TAG, "Download Progress: " + downloadProgress + "%");
+            int finalDownloadProgress = downloadProgress;
+
+
+            new Handler(Looper.getMainLooper()).post(new Runnable(){
+                @Override
+                public void run() {
+                    textView.setText("Download Progress: " + finalDownloadProgress + "%");
+                }
+            });
+
+            try{
+                Thread.sleep(1000);
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                startButton.setText("START");
+                new Handler(Looper.getMainLooper()).post(new Runnable(){
+                    @Override
+                    public void run() {
+                        textView.setText("");
+                    }
+                });
+            }
+        });
+    }
+
+    public void startDownload(View view){
+        stopThread = false;
+        ExampleRunnable runnable = new ExampleRunnable();
+        new Thread(runnable).start();
+    }
+
+    public void stopDownload(View view){
+        stopThread = true;
+        new Handler(Looper.getMainLooper()).post(new Runnable(){
+            @Override
+            public void run() {
+                textView.setText("");
+            }
+        });
+    }
+    public class ExampleRunnable implements Runnable{
+        // private static final String TAG = "MainActivity";
+        @Override
+        public void run(){
+            mockFiledownloader();
+        }
+    }
+}
